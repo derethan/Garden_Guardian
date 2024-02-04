913651c2ec7468c2c6596e1ed9aad2573d183b82
Andrew Patterson <apatterson93@gmail.com>
2024-02-04 16:34:13 -0330

    Add Register page to siteLinks array

---
+++ src/views/Register.jsx
@@ -0,0 +1,15 @@
+/************************************************ 
+*   Account Registration page for the application
+*************************************************/
+
+// Import necessary libraries
+import { Container } from '@mui/material';
+
+
+export default function ResetPassword() {
+    return (
+        <Container maxWidth='sm' sx={{paddingTop: 4}}>
+        </Container>
+
+    );
+}
