35f01c97eb60297fabfe2ae43bacd4b6c82559a0
Andrew Patterson <apatterson93@gmail.com>
2024-02-04 15:12:29 -0330

    Refactor siteTitle component to use styled RouterLink

--- src/components/header/siteTitle.jsx
+++ src/components/header/siteTitle.jsx
@@ -1,18 +1,30 @@
-import { Box, Typography } from '@mui/material';
+import { Box } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { styled } from "@mui/material";
+
+const StyledTitleLink = styled(RouterLink)(({ theme }) => ({
+  fontFamily: theme.typography.title.main, //font
+  color: theme.palette.primary.main,
+  textShadow: "1px 1px 4px #000000",
+  textDecoration: "none",
+  fontSize: "2rem",
+  paddingLeft: "40px",
+
+  "&:hover": {
+    color: theme.palette.text.primary,
+    textDecoration: "none",
+  },
+  // [theme.breakpoints.down('md')]: {
+  //     fontSize: '1.5rem',
+  // },
+}));
 
 function SiteTitle() {
-    return (
-        <Box>
-            <Typography variant='h3' component='h1' sx={{
-                fontFamily: (theme) => theme.typography.title.main,
-                color: (theme) => theme.typography.primary.main,
-                marginTop: '-40px',
-                textShadow: '1px 1px 4px #000000',
-            }}>
-                Garden Guardian
-            </Typography>
-        </Box>
-    );
+  return (
+    <Box sx={{ marginTop: "-40px" }}>
+      <StyledTitleLink to="/">Garden Guardian</StyledTitleLink>
+    </Box>
+  );
 }
 
 export default SiteTitle;
