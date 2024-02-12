b9539a2dddc8109553cd673ccc86b437f6e6aa9c
Andrew Patterson <apatterson93@gmail.com>
2024-02-12 16:00:57 -0330

    Refactor: Moved inputs into seperate components

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,12 +1,18 @@
 //Import necessary libraries
-import { Box, Button, Card, TextField, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Box, Button, Card, Typography } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 import { useValidate } from "../hooks/useValidate";
 import { usePostRequest } from "../hooks/usePostRequest";
 
+import AccountDetailsInput from "./account/accountDetailsInput";
+import PasswordWithConfirmInput from "./account/PasswordWithConfirmInput";
+
+// Function to handle the registration form
 const RegisterForm = () => {
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -15,39 +21,50 @@ const RegisterForm = () => {
     confirmPassword: "",
   });
 
-  const [formErrors, validateForm] = useValidate(formData);
   const [isRegistered, setIsRegistered] = useState(false);
+
+  const [formErrors, validateForm] = useValidate(formData);
   const [postStatus, postMessage, setPostMessage, postData] = usePostRequest();
 
   // Function to handle the form input changes, Updates the formdata state with the new value
   const handleChange = (event) => {
     setFormData({ ...formData, [event.target.name]: event.target.value });
 
-    if (event.target.name === 'email') {
-      setPostMessage("");
+    if (event.target.name === "email") {
+      setPostMessage(""); // Removes duplicate Email warning
     }
-    
-
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
+    console.log("Form Data: ", formData);
+
     // If the form is valid, Post the data to the server
     if (validateForm()) {
       const URL = import.meta.env.VITE_API_URL;
       // Post the data to the server
-      const success = await postData(URL + 
-        "users/register",
-        formData
-      );
+      const success = await postData(URL + "users/register", formData);
 
-      console.log("register status: ", success);
-      setIsRegistered(success);
+      // If the user is registered, redirect to the login page
+      if (success) {
+        console.log("register status: ", success);
+        setIsRegistered(success);
+
+        setFormData({
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        });
+
+        navigate("/login");
+      }
+    } else {
+      console.log("Form is invalid");
     }
   };
-
-
 
   return (
     <Card variant="dark" sx={{ padding: 2, margin: 2 }}>
@@ -57,114 +74,22 @@ const RegisterForm = () => {
         autoComplete="off"
         onSubmit={handleSubmit}
       >
-        <Typography variant="subtitle1" color="text.card">
-          Account Details
-        </Typography>
+        <AccountDetailsInput
+          formData={formData}
+          formErrors={formErrors}
+          handleChange={handleChange}
+        />
+        {postStatus === 409 && (
+          <Typography variant="body1" color="error">
+            {postMessage}
+          </Typography>
+        )}
 
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: 2,
-            padding: 2,
-          }}
-        >
-          <TextField
-            required
-            label="First Name"
-            name="firstName"
-            id="firstName"
-            variant="outlined"
-            size="small"
-            sx={{
-              backgroundColor: "background.default",
-            }}
-            value={formData.firstName}
-            onChange={handleChange}
-            error={formErrors.firstName ? true : false}
-            helperText={formErrors.firstName}
-          />
-          <TextField
-            required
-            label="Last Name"
-            name="lastName"
-            id="lastName"
-            variant="outlined"
-            size="small"
-            sx={{
-              backgroundColor: "background.default",
-            }}
-            value={formData.lastName}
-            onChange={handleChange}
-            error={formErrors.lastName ? true : false}
-            helperText={formErrors.lastName}
-          />
-          <TextField
-            label="Email"
-            type="email"
-            name="email"
-            id="email"
-            variant="outlined"
-            size="small"
-            sx={{
-              backgroundColor: "background.default",
-            }}
-            value={formData.email}
-            onChange={handleChange}
-            error={formErrors.email ? true : false}
-            helperText={formErrors.email}
-          />
-
-          {postStatus === 409 && (
-            <Typography variant="body1" color="error">
-              {postMessage}
-            </Typography>
-          )}
-
-        </Box>
-
-        <Typography variant="subtitle1" color="text.card">
-          Password Details
-        </Typography>
-
-        <Box
-          sx={{
-            display: "flex",
-            gap: 2,
-            padding: 2,
-          }}
-        >
-          <TextField
-            label="Password"
-            type="password"
-            name="password"
-            id="password"
-            variant="outlined"
-            size="small"
-            sx={{
-              backgroundColor: "background.default",
-            }}
-            value={formData.password}
-            onChange={handleChange}
-            error={formErrors.password ? true : false}
-            helperText={formErrors.password}
-          />
-          <TextField
-            label="Confirm Password"
-            type="password"
-            name="confirmPassword"
-            id="confirmPassword"
-            variant="outlined"
-            size="small"
-            sx={{
-              backgroundColor: "background.default",
-            }}
-            value={formData.confirmPassword}
-            onChange={handleChange}
-            error={formErrors.confirmPassword ? true : false}
-            helperText={formErrors.confirmPassword}
-          />
-        </Box>
+        <PasswordWithConfirmInput
+          formData={formData}
+          formErrors={formErrors}
+          handleChange={handleChange}
+        />
 
         <Box>
           <Button
@@ -177,12 +102,6 @@ const RegisterForm = () => {
             Register Account
           </Button>
         </Box>
-
-        {isRegistered && (
-          <Typography variant="body1" color="text.primary">
-            Registration Successful
-          </Typography>
-        )}
 
         <Link to="/login">
           <Typography
---
+++ src/components/account/accountDetailsInput.jsx
@@ -0,0 +1,69 @@
+import { Box, TextField, Typography } from "@mui/material";
+
+const AccountDetailsInput = ({formData, formErrors, handleChange}) => {
+  return (
+    <>
+      <Typography variant="subtitle1" color="text.card">
+        Account Details
+      </Typography>
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+          padding: 2,
+        }}
+      >
+        <TextField
+          required
+          label="First Name"
+          name="firstName"
+          id="firstName"
+          variant="outlined"
+          size="small"
+          sx={{
+            backgroundColor: "background.default",
+          }}
+          value={formData.firstName}
+          onChange={handleChange}
+          error={formErrors.firstName ? true : false}
+          helperText={formErrors.firstName}
+        />
+        <TextField
+          required
+          label="Last Name"
+          name="lastName"
+          id="lastName"
+          variant="outlined"
+          size="small"
+          sx={{
+            backgroundColor: "background.default",
+          }}
+          value={formData.lastName}
+          onChange={handleChange}
+          error={formErrors.lastName ? true : false}
+          helperText={formErrors.lastName}
+        />
+        <TextField
+          label="Email"
+          type="email"
+          name="email"
+          id="email"
+          variant="outlined"
+          size="small"
+          sx={{
+            backgroundColor: "background.default",
+          }}
+          value={formData.email}
+          onChange={handleChange}
+          error={formErrors.email ? true : false}
+          helperText={formErrors.email}
+        />
+      </Box>
+    </>
+  );
+};
+
+
+export default AccountDetailsInput;
