ea2f836e590b8430c4ece571a7ca78a243070d00
Andrew Patterson <apatterson93@gmail.com>
2024-03-02 02:51:38 -0330

    Update AddDevice component with form and submit functionality

--- src/components/modals/AddDevice.jsx
+++ src/components/modals/AddDevice.jsx
@@ -1,4 +1,4 @@
-import { Card, Container } from "@mui/material";
+import { Box, Card, Container } from "@mui/material";
 import "./modals.css";
 import {
   DialogTitle,
@@ -12,11 +12,49 @@ import DeviceUnknownIcon from "@mui/icons-material/DeviceUnknown";
 
 import { PrimaryButton } from "../../components/PrimaryButton";
 
+import { useState } from "react";
+
+import { usePostRequest } from "../../hooks/usePostRequest";
+
 const AddDevice = ({ display, setShowAddDeviceModal }) => {
+  const [deviceData, setDeviceID] = useState({
+    device_id: "",
+    device_name: "",
+  });
+
+  //Create the post request hook
+  const [postStatus, postMessage, , , postData] = usePostRequest();
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setDeviceID((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (event) => {
+    const URL = import.meta.env.VITE_API_URL;
+
+    event.preventDefault();
+
+    // Post the device data to the API
+    try {
+      postData(URL + 'users/addDevice', deviceData);
+    }
+    catch (error) {
+      console.error(error);
+    }
+
+    console.log(`Device ID: ${JSON.stringify(deviceData)}`);
+  };
+
+  // If the display prop is false, Do not show Modal
   if (!display) {
     return null;
   }
-
   return (
     <div className="add-device-wrapper">
       <Container
@@ -32,31 +70,55 @@ const AddDevice = ({ display, setShowAddDeviceModal }) => {
             padding: 2,
             maxWidth: { md: "100%", lg: "100%" },
           }}
+          component="form"
+          onSubmit={handleSubmit}
+          noValidate
         >
-          <DeviceUnknownIcon fontSize="large" sx={{color: 'green'}} />
+          <DeviceUnknownIcon fontSize="large" sx={{ color: "green" }} />
           <DialogTitle>Add a Device</DialogTitle>
           <DialogContent>
-            <DialogContentText sx={{paddingBottom: 2}}>
+            <DialogContentText sx={{ paddingBottom: 2 }}>
               {
                 "Oops! It looks like you haven't added a device yet. Please enter your Device ID to get started."
               }
             </DialogContentText>
-            <TextField
-              autoFocus
-              required
-              fullWidth
-              id="deviceIDField"
-              name="deviceIDfield"
-              label="Device ID"
-              type="text"
-              margin="normal"
-              color="primary"
-              size="small"
-              variant="outlined"
-            />
+            <Box sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+            
+            }}>
+              <TextField
+                id="deviceIDField"
+                name="device_id"
+                label="Device ID"
+                type="text"
+                margin="normal"
+                color="primary"
+                size="small"
+                variant="outlined"
+                value={deviceData.ID}
+                onChange={handleChange}
+              />
+              <TextField
+                id="deviceNameField"
+                name="device_name"
+                label="Enter a Name for your Device"
+                type="text"
+                margin="normal"
+                color="primary"
+                size="small"
+                variant="outlined"
+                value={deviceData.Name}
+                onChange={handleChange}
+              />
+            </Box>
           </DialogContent>
-          <DialogActions sx={{justifyContent:'center'}}>
-            <PrimaryButton text="Cancel" onClick={() => setShowAddDeviceModal(false)} />
+          <DialogActions sx={{ justifyContent: "center" }}>
+            <PrimaryButton
+              text="Cancel"
+              onClick={() => setShowAddDeviceModal(false)}
+            />
             <PrimaryButton text="Submit" type="submit" />
           </DialogActions>
         </Card>
