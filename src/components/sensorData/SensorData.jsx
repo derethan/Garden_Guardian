5b1a94c8307c8590565e7b64edcd19674b9a02bd
Andrew Patterson <apatterson93@gmail.com>
2024-03-20 22:03:32 -0230

    BugFix: - added conditional check if previous sensorData exists before checking for difference in temperatures.

--- src/components/sensorData/SensorData.jsx
+++ src/components/sensorData/SensorData.jsx
@@ -35,31 +35,31 @@ const SensorData = () => {
 
   //Retrieves the Latest Sensor Data from the API
   useEffect(() => {
-
     //Retrieves the Latest Sensor Data from the API
     async function getSensorData() {
       const data = await getLatestReading(deviceID);
 
-      //Retrieves the Previous Sensor Data from the Local Storage
-      const storedData = JSON.parse(localStorage.getItem("sensorData"));
-      const difference = calculateDifference(data, storedData);
+      if (localStorage.getItem("sensorData")) {
+        //Retrieves the Previous Sensor Data from the Local Storage
+        const storedData = JSON.parse(localStorage.getItem("sensorData"));
+        const difference = calculateDifference(data, storedData);
+        setDifference(difference);
+      }
 
       //Stores the Current Sensor Data and the Difference in the current and previous reading
       localStorage.setItem("sensorData", JSON.stringify(data));
       setSensorData(data);
-      setDifference(difference);
     }
 
     getSensorData();
 
     //Set the interval to check the device every 1 minutes
-    const interval =  setInterval(() => {
+    const interval = setInterval(() => {
       getSensorData();
     }, 1 * 60 * 1000);
 
     //Clear the interval when the component is unmounted
     return () => clearInterval(interval);
-
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   //Renders the Sensor Data Page
