87ca6c8d71c95df71ae377c73d6256aa659df2ad
Andrew Patterson <apatterson93@gmail.com>
2024-04-05 15:10:15 -0230

    Refactor Dashboard component to add SensorSummaryColumn component

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,25 +1,46 @@
 import { useAuth } from "../hooks/useAuthProvider";
-import { Container, Box, Typography } from "@mui/material";
-
+import { Container, Box, Typography, Divider } from "@mui/material";
+import SummaryColumn from "../components/sensorData/SummaryColumn";
 
 const Dashboard = () => {
-  const user = useAuth();
-  const name = user.user.name.split(" ")[0];
+  const { user, hasDevice } = useAuth();
+  const name = user.name.split(" ")[0];
 
   return (
-      <Container maxWidth="none" sx={{ paddingTop: 4 }}>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "flex-start",
-            alignItems: "center",
-            gap: 2,
-          }}
-        >
-          <Typography variant="h4">Welcome, {name}!</Typography>
-        </Box>
+    <Container
+      maxWidth="none"
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "flex-start",
+        paddingTop: 4,
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          gap: 2,
+          width: hasDevice ? "70%" : "100%",
+        }}
+      >
+        <Typography variant="h4">Welcome, {name}!</Typography>
+      </Box>
 
-      </Container>
+      {hasDevice && (
+        <Box sx={{ width: "30%", display: { xs: "none", md: "flex" }, gap: 2 }}>
+          <Divider
+            orientation="vertical"
+            flexItem
+            sx={{
+              display: { xs: "none", md: "block" },
+            }}
+          />
+          <SummaryColumn />
+        </Box>
+      )}
+    </Container>
   );
 };
 
