87e51d8df60157add4e7ffad4ba0ecbea2227bb6
Andrew Patterson <apatterson93@gmail.com>
2024-04-06 14:07:52 -0230

    Refactor $plant.jsx to include Breadcrumbs and Link components

--- src/views/$plant.jsx
+++ src/views/$plant.jsx
@@ -1,14 +1,34 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from "react-router-dom";
+import { Container, Box, Breadcrumbs, Typography } from "@mui/material";
+
+import { useState } from "react";
 
 export const Plant = () => {
-    let { plantID } = useParams();
+  let { plantID } = useParams();
 
-    console.log(plantID);
+  const storedPlants = JSON.parse(localStorage.getItem("gardenPlants"));
+  const initialPlantData = storedPlants
+    ? storedPlants.filter((plant) => plant.plantID === plantID)
+    : [];
+  const [plantData, setPlantData] = useState(initialPlantData[0] || {});
 
-    return (
-        <div>
-            <h1>Plant: </h1> {plantID}
-        </div>
-    )
-}
+  console.log(plantData);
 
+  return (
+    <Container>
+      <Box pt={4}>
+        <Breadcrumbs aria-label="breadcrumb">
+          <Link color="inherit" to="/">
+            Home
+          </Link>
+          <Link color="inherit" to="/gardens">
+            Crop Management
+          </Link>
+          <Typography color="textPrimary">
+            {plantData.name || plantData.commonName || plantData.scientificName}
+          </Typography>
+        </Breadcrumbs>
+      </Box>
+    </Container>
+  );
+};
