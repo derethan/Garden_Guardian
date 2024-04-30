9ed47edae637cc3c617b7d4625fbfb96f2121d92
Andrew Patterson <apatterson93@gmail.com>
2024-04-30 21:20:21 -0230

    Updates

--- assets/PrimaryButton-n76tX089.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-fXEaP3-y.js";import{B as a}from"./Button-MmoEpRVj.js";const s=({fullWidth:t,text:i,...r})=>o.jsxs(a,{...r,fullWidth:t,variant:"contained",color:"primary",sx:{borderRadius:20,opacity:r.opacity,marginTop:2,...r.sx},children:[r.children,i]});export{s as P};
