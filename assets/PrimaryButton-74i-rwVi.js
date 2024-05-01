d42edca3732a96ffc0c4a269b072b0da1a0a9ce4
Andrew Patterson <apatterson93@gmail.com>
2024-04-30 23:17:44 -0230

    Updates

--- assets/PrimaryButton-74i-rwVi.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-P0Wgf1Vu.js";import{B as a}from"./Button-yNbzG7T7.js";const s=({fullWidth:t,text:i,...r})=>o.jsxs(a,{...r,fullWidth:t,variant:"contained",color:"primary",sx:{borderRadius:20,opacity:r.opacity,marginTop:2,...r.sx},children:[r.children,i]});export{s as P};
