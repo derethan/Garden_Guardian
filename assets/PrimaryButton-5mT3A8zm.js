ea1026016c1b3443f6962cc4ef76874ca30be973
Andrew Patterson <apatterson93@gmail.com>
2024-05-05 14:05:30 -0230

    Updates

--- assets/PrimaryButton-5mT3A8zm.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-yESpCeQD.js";import{B as a}from"./Card-atDsQMDj.js";const s=({fullWidth:t,text:i,...r})=>o.jsxs(a,{...r,fullWidth:t,variant:"contained",color:"primary",sx:{borderRadius:20,opacity:r.opacity,marginTop:2,...r.sx},children:[r.children,i]});export{s as P};
