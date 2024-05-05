865b40c78b17e8d3786aaf56fd4fe844eede9311
Andrew Patterson <apatterson93@gmail.com>
2024-05-05 02:58:02 -0230

    Updates

---
+++ assets/PrimaryButton-WpqmAVA4.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-0-DzTY72.js";import{B as a}from"./Button-Km4cH45B.js";const s=({fullWidth:t,text:i,...r})=>o.jsxs(a,{...r,fullWidth:t,variant:"contained",color:"primary",sx:{borderRadius:20,opacity:r.opacity,marginTop:2,...r.sx},children:[r.children,i]});export{s as P};
