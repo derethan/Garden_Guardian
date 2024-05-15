e96da8b84d799c6b1c329620974d78524038d5c6
Andrew Patterson <apatterson93@gmail.com>
2024-05-15 13:30:24 -0230

    Update about.jsx
    Added About the author and a personal Memo

--- src/views/about.jsx
+++ src/views/about.jsx
@@ -1,6 +1,6 @@
 import LandingSiteNav from "../components/nav/LandingSiteNav";
 
-import { Container, Grid, Card, CardContent, Typography } from "@mui/material";
+import { Container, Grid, Card, CardContent, Typography, Box } from "@mui/material";
 
 const About = () => {
   return (
@@ -15,9 +15,7 @@ const About = () => {
                 <Typography
                   variant="h4"
                   align="center"
-                  sx={{ color: "text.main",
-                fontFamily: "title.main",
-              }}
+                  sx={{ color: "text.main", fontFamily: "title.main" }}
                 >
                   Garden Guardian
                 </Typography>
@@ -66,11 +64,12 @@ const About = () => {
                 </Typography>
                 <Typography variant="body2" align="left" pt={4} p={2}>
                   In times of rising living costs, we believe in equipping
-                  individuals with the tools to be more self-reliant. <br /><br /> Our
-                  platform makes small-scale farming accessible to all, enabling
-                  people to grow their own food easily. <br /><br />This not only alleviates
-                  financial burdens but also allows everyone to contribute to
-                  environmental conservation.
+                  individuals with the tools to be more self-reliant. <br />
+                  <br /> Our platform makes small-scale farming accessible to
+                  all, enabling people to grow their own food easily. <br />
+                  <br />
+                  This not only alleviates financial burdens but also allows
+                  everyone to contribute to environmental conservation.
                 </Typography>
               </CardContent>
             </Card>
@@ -110,6 +109,93 @@ const About = () => {
                   Currently in the development phase, we are actively refining
                   our platform to meet the needs of both home-based enthusiasts
                   and greenhouse operators.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+
+        <Box sx={{ pt: 8, pb: 6 }}>
+          <Typography variant="h4" align="center" sx={{ color: "text.main" }}>
+            Hi, I'm Andrew
+          </Typography>
+          <Typography
+            variant="caption"
+            align="center"
+            color={"text.subtitle"}
+            fontWeight={"bold"}
+          >
+            (The developer behind Garden Guardian)
+          </Typography>
+        </Box>
+
+        <Grid container spacing={3} pb={4} justifyContent={"center"}>
+          <Grid item xs={12} md={6}>
+            <Card variant="light">
+              <CardContent>
+                <Typography
+                  variant="h6"
+                  align="center"
+                  sx={{ color: "text.main" }}
+                >
+                  The Story Behind Garden Guardian
+                </Typography>
+                <Typography variant="body2" align="left" pt={4} p={2}>
+                  My Roomate and Myself started Garden Guardian as a simple IoT
+                  Device and webpage to assist in tracking the conditions of our
+                  Gardens and Greenhouse.
+                  <br />
+                  <br />
+                  Over the course of my React Module with Get Coding it has
+                  quickly evolved into a IoT enabled platform that combines
+                  Realtime Sensor data with an easy to use interface to track
+                  garden related information and plant data.
+                  <br />
+                  <br />
+                  This has removed the need for manual record keeping and has
+                  saved allot of my time and effort on what would have otherwise
+                  been repetitive daily tasks, allowing me to focus on what
+                  matters most, the plants.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <Card variant="light">
+              <CardContent>
+                <Typography
+                  variant="h6"
+                  align="center"
+                  sx={{ color: "text.main" }}
+                >
+                  A Personal Note on Gardening
+                </Typography>
+                <Typography variant="body2" align="left" pt={4} p={2}>
+                  I myself feel that many of us have become overly reliant on
+                  large retailers and grocery chains as our only way to get by.
+                  leading to rising costs that are unsustainable for many of us.
+                  <br />
+                  <br />
+                  Growing our own food can reduce our dependence on expensive
+                  groceries and provide us with a sense of security. This simple
+                  approach has been practiced by our grandparents and
+                  generations before them throughout history.
+                  <br />
+                  <br />
+                  For those interested in growing their own food, here's some
+                  helpful words of advice:
+                  <br />
+                  <br />
+                  <span
+                    style={{
+                      fontStyle: "italic",
+                      color: "#555",
+                    }}
+                  >
+                    "Turn any available property into a productive space.
+                    Instead of growing grass, grow something that you can use."
+                  </span>
                 </Typography>
               </CardContent>
             </Card>
